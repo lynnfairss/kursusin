764dd3914ec70e357f2cf8f6e669f45d457154cb
noname87productions <noname87productions@gmail.com>
2024-12-11 13:44:03 +0700

    by nim 23431144

---
+++ app/src/main/java/com/example/nim234311044/kursusapps/DataSource.kt
@@ -0,0 +1,30 @@
+package com.example.nim234311044.kursusapps
+
+object DataSource {
+    val topics = listOf(
+        Topic(R.string.architecture, 58, R.drawable.kursus),
+        Topic(R.string.automotive, 30, R.drawable.automotive),
+        Topic(R.string.biology, 90, R.drawable.teach),
+        Topic(R.string.crafts, 121, R.drawable.training_3185170_1280),
+        Topic(R.string.business, 78, R.drawable.olga),
+        Topic(R.string.culinary, 118, R.drawable.culinary),
+        Topic(R.string.design, 423, R.drawable.photography  ),
+        Topic(R.string.ecology, 28, R.drawable.kursus),
+        Topic(R.string.engineering, 67, R.drawable.automotive),
+        Topic(R.string.fashion, 92, R.drawable.fashion),
+        Topic(R.string.finance, 100, R.drawable.finance),
+        Topic(R.string.film, 165, R.drawable.film),
+        Topic(R.string.gaming, 37, R.drawable.gaming),
+        Topic(R.string.geology, 290, R.drawable.photography),
+        Topic(R.string.drawing, 326, R.drawable.drawing),
+        Topic(R.string.history, 189, R.drawable.history),
+        Topic(R.string.journalism, 96, R.drawable.kursus),
+        Topic(R.string.law, 58, R.drawable.fashion),
+        Topic(R.string.lifestyle, 305, R.drawable.olga),
+        Topic(R.string.music, 212, R.drawable.gaming),
+        Topic(R.string.painting, 172, R.drawable.film),
+        Topic(R.string.photography, 321, R.drawable.photography),
+        Topic(R.string.physics, 41, R.drawable.physics),
+        Topic(R.string.tech, 118, R.drawable.teach),
+    )
+}
---
+++ app/src/main/java/com/example/nim234311044/kursusapps/Topic.kt
@@ -0,0 +1,7 @@
+package com.example.nim234311044.kursusapps
+
+data class Topic(
+    val name: Int,                // ID resource string
+    val availableCourses: Int,    // Jumlah kursus
+    val imageRes: Int             // ID resource drawable
+)
